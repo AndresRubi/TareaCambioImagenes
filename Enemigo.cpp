8b7260be7de6b8e5d6fbcf024e72dad412e1fb1b
AndresRubi <andresrues@hotmail.com>
2014-08-22 18:20:03 -0600

    first commit

---
+++ Enemigo.cpp
@@ -0,0 +1,27 @@
+#include "Enemigo.h"
+
+Enemigo::Enemigo(SDL_Renderer* renderer)
+{
+    this-> renderer=renderer;
+    textura = IMG_LoadTexture(renderer, "enemigo1.png");
+    SDL_QueryTexture(textura, NULL, NULL, &rectangulo.w, &rectangulo.h);
+    rectangulo.x = 2;
+    rectangulo.y = 2;
+
+}
+
+Enemigo::~Enemigo()
+{
+    //dtor
+}
+
+void Enemigo::dibujar()
+{
+    SDL_RenderCopy(renderer, textura, NULL, &rectangulo);
+
+}
+
+void Enemigo::logica()
+{
+    rectangulo.x++;
+}
---
+++ Enemigo.h
@@ -0,0 +1,22 @@
+#ifndef ENEMIGO_H
+#define ENEMIGO_H
+#include<SDL2/SDL_image.h>
+
+#include <SDL2/SDL.h>
+
+
+class Enemigo
+{
+    public:
+        SDL_Renderer* renderer;
+        SDL_Texture* textura;
+        SDL_Rect rectangulo;
+        void logica();
+        void dibujar();
+        Enemigo(SDL_Renderer* renderer);
+        virtual ~Enemigo();
+    protected:
+    private:
+};
+
+#endif // ENEMIGO_H
